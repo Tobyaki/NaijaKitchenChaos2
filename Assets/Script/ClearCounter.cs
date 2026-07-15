7d752e6919f4b372c293f667981947697d9966ef
ducheanidi-designs <ducheanidi@gmail.com>
2026-07-15 18:57:10 +0100

    cooking mech.- more updates

--- Assets/Script/ClearCounter.cs
+++ Assets/Script/ClearCounter.cs
@@ -2,64 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour, IKitchenObjectParent
+public class ClearCounter : BaseCounter
 {
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
-    [SerializeField] private Transform counterTopPoint;
-    [SerializeField] private ClearCounter secondclearCounter;
-    [SerializeField] private bool testing;
 
-    private KitchenObject kitchenObject;
-
-    private void Update()
+   
+    public override void Interact(Player player)
     {
-        if (testing && Input.GetKeyDown(KeyCode.T))
-        {
-            if (kitchenObject != null)
-            {
-                kitchenObject.SetKitchenObjectParent(secondclearCounter);
-            
+        if (!HasKitchenObject())
+        { //There is no KitchenObject here
+            if (player.HasKitchenObject())
+            {//Player is carrying something
+             player.GetKitchenObject().SetKitchenObjectParent(this);
+            } else {//Player not carrying anything
             }
         }
-    }
+        else {//There is a KitchenObject here
+            if (player.HasKitchenObject())
+            {//Player is carrying something
+            } else {//Player is not carrying anything
+              GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
 
-    public void Interact(Player player)
-    {
-        if (kitchenObject == null)
-        {
-            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
-            
-        }
-        else
-        {   //Give the object to the player
-            kitchenObject.SetKitchenObjectParent(player);
-        }
     }
       
-    public Transform GetKitchenObjectFollowTransform()
-    {
-        return counterTopPoint;
-    }
-    public void SetkitchenObject (KitchenObject kitchenObject)
-    {
-        this.kitchenObject = kitchenObject; 
-    }
 
-    public KitchenObject GetKitchenObject()
-    {
-        return kitchenObject;
-    }
-
-    public void ClearKitchenObject()
-    {
-        kitchenObject = null;
-    }
-
-    public bool HasKitchenObject()
-    {
-        return kitchenObject != null;
-    }
     
 }
