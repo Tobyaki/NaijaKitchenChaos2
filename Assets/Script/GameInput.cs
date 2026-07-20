2e0f52e533e97358792a0289133573ff58bb4bee
joshuaibeneme04-lgtm <joshuaibeneme04@gmail.com>
2026-07-20 09:42:32 +0100

    New stuff

--- Assets/Script/GameInput.cs
+++ Assets/Script/GameInput.cs
@@ -11,6 +11,7 @@ public class GameInput : MonoBehaviour
     public static GameInput Instance { get; private set; }
 
     public event EventHandler OnInteractAction;
+    public event EventHandler OnInteractAlternateAction;
     public event EventHandler OnPauseAction;
 
     public enum Binding
@@ -39,12 +40,18 @@ public class GameInput : MonoBehaviour
         playerInputActions.Player.Enable();
 
         playerInputActions.Player.Interact.performed += Interact_performed;
+        playerInputActions.Player.InteractAlternate.performed += InteractAlternate_performed;
         playerInputActions.Player.Pause.performed += Pause_performed;
     }
 
+    private void InteractAlternate_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+       OnInteractAlternateAction?.Invoke(this, EventArgs.Empty);
+    }
     private void OnDestroy()
     {
         playerInputActions.Player.Interact.performed -= Interact_performed;
+        playerInputActions.Player.InteractAlternate.performed -= InteractAlternate_performed;
         playerInputActions.Player.Pause.performed -= Pause_performed;
 
         playerInputActions.Dispose();
