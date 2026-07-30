1936ee45527e0bdb00364e6e9973edddfdae3c57
ducheanidi-designs <ducheanidi@gmail.com>
2026-07-30 16:20:53 +0100

    More updates from Nduche

---
+++ Assets/Script/IHasProgress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IHasProgress
+{
+    // Remove the "public" keyword here (it is implicitly public in C# 7.3)
+    event EventHandler<OnProgressChangedEventArgs> OnProgressChanged;
+
+    void Interact(Player player);
+}
+
+// Move this class completely outside of the interface
+public class OnProgressChangedEventArgs : EventArgs
+{
+    public float progressNormalized;
+}
+
